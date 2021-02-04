72a4113aa2ea753b77fd41cd45dea6d5acc7ae2b
Nurlan <nurlaney@code.edu.az>
2021-02-04 23:10:29 +0400

    production setting

--- Gemfile
+++ Gemfile
@@ -3,7 +3,13 @@ source 'http://rubygems.org'
 gem 'sinatra'
 gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
-gem 'sqlite3'
 gem 'rake'
 gem 'puma'
 gem 'require_all'
+group :development do
+ gem 'sqlite3'
+end
+
+group :production do
+ gem 'pg'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,23 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-set :database, {adapter: "sqlite3", database: "db/#{ENV['SINATRA_ENV']}.sqlite"}
+configure :development do
+    set :database, {adapter: "sqlite3", database: "db/#{ENV['SINATRA_ENV']}.sqlite"}
+    set :show_exceptions, true
+   end
+   
+   configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+   
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+   end
 
 
 require './app/controllers/application_controller'
